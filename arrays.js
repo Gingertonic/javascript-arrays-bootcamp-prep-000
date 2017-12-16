6152b58c061ae30df615e369e81f0efccbe4add7
Beth Schofield <bethmschofield@gmail.com>
2017-12-16 08:53:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
 
-// function addElementToBeginningOfArray(arr, toAdd) {
-//   var newArray = [ ...arr];
-//   newArray.unshift(toAdd)];
-// }
+function addElementToBeginningOfArray(arr, toAdd) {
+  var newArray = [ ...arr];
+  newArray.unshift(toAdd)];
+}
 //
 // function  destructivelyAddElementToBeginningOfArray(arr, toAdd) {
 //   arr.unshift(toAdd);
